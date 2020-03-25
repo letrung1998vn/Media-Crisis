e8ccb261be43089da983971b242c1c5d87ac8433
s0mg2703 <32416650+s0mg2703@users.noreply.github.com>
2020-03-25 17:34:42 +0700

    MinhDN
    JS call API save Token for user

---
+++ src/java/MediaCrisis/Controller/User/UpdateNotiBrowserTokenController.java
@@ -0,0 +1,127 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MediaCrisis.Controller.User;
+
+import MediaCrisis.APIConnection.APIConnection;
+import MediaCrisis.Model.User;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import org.json.JSONObject;
+
+/**
+ *
+ * @author Administrator
+ */
+@WebServlet(name = "UpdateNotiBrowserToken", urlPatterns = {"/UpdateNotiBrowserToken"})
+public class UpdateNotiBrowserTokenController extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    private final String error = "error.html";
+    private final String mainPage = "mainPage_JSP.jsp";
+    private final String login = "login_JSP.jsp";
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            String token = request.getParameter("txtToken");
+            HttpSession session = request.getSession();
+            int resultCode = 0;
+            String result = "";
+            String nextPage = "";
+
+            List<String> params = new ArrayList<>();
+            List<String> value = new ArrayList<>();
+            String url = "http://localhost:8181/user/updateNotiToken/?";
+
+            params.add("token");
+            params.add("username");
+            value.add(token);
+            value.add(session.getAttribute("USERID").toString());
+
+            //Call API connection and get return JSON string
+            APIConnection ac = new APIConnection(url, params, value);
+            result = ac.connect();
+
+            try {
+                JSONObject jsonResult = new JSONObject(result);
+                session.setAttribute("CREATE_MESSAGE", jsonResult.get("statusMessage"));
+                resultCode = Integer.parseInt(jsonResult.get("statusCode").toString());
+                session.setAttribute("RESULT", resultCode);
+                session.setAttribute("SEND", true);
+                if (resultCode == 3) {
+                    nextPage = login;
+                } else {
+                    User user = (User) session.getAttribute("USERLOGIN");
+                    user.setLink_webhook(jsonResult.get("obj").toString());
+                    session.setAttribute("USERLOGIN", user);
+                    nextPage = mainPage;
+                }
+            } catch (Exception e) {
+                System.out.println("Ko parse duoc json object");
+            }
+            RequestDispatcher rd = request.getRequestDispatcher(nextPage);
+            rd.forward(request, response);
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
--- web/Notification.jsp
+++ web/Notification.jsp
@@ -12,6 +12,22 @@
         <title>JSP Page</title>
     </head>
     <body>
+        <form id="form-update-token" action="UpdateNotiBrowserTokenController" method="post" class="">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="form-group">
+                        <div class="col-md-12">
+                            <div class="col-md-4"><label>Your noti token:</label></div>
+                        </div>
+                        <div class="col-md-12">
+                            <div class="col-md-9">
+                                <input id="tokenString" type="text" name="txtToken" class="form-control" placeholder="Token link">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </form>
         <div id="token"></div>
         <div id="msg"></div>
         <div id="notis"></div>
@@ -25,6 +41,9 @@
         <script src="https://www.gstatic.com/firebasejs/7.12.0/firebase-app.js"></script>
         <script src="https://www.gstatic.com/firebasejs/7.12.0/firebase-messaging.js"></script>
         <script>
+            var tokenForm = document.getElementById("form-update-token");
+            var tokenString = document.getElementById("tokenString");
+            
             var firebaseConfig = {
                 apiKey: "AIzaSyDu0alR16fuDysOrsWMWF9bm-IkscLH4Zw",
                 authDomain: "media-crisis.firebaseapp.com",
@@ -37,29 +56,31 @@
             };
             // Initialize Firebase
             firebase.initializeApp(firebaseConfig);
-            const messaging = firebase.messaging();
-            navigator.serviceWorker.register('./js/firebase-messaging-sw.js')
-                    .then((registration) => {
-                        messaging.useServiceWorker(registration);
-                        // Request permission and get token.....
-                        messaging
-                                .requestPermission()
-                                .then(function () {
-                                    MsgElem.innerHTML = "Notification permission granted."
-                                    console.log("Notification permission granted.");
-                                    // get the token in the form of promise
-                                    return messaging.getToken()
-                                })
-                                .then(function (token) {
-                                    // print the token on the HTML page
-                                    TokenElem.innerHTML = "token is : " + token
-                                })
-                                .catch(function (err) {
-                                    console.log("Unable to get permission to notify.", err);
-                                });
-                        messaging.onMessage(function (payload) {
-                            console.log("Message received. ", payload);
-                        });
+                    const messaging = firebase.messaging();
+                    navigator.serviceWorker.register('./js/firebase-messaging-sw.js')
+                    .then((registration) = > {
+                    messaging.useServiceWorker(registration);
+                    // Request permission and get token.....
+                    messaging
+                            .requestPermission()
+                            .then(function () {
+                                MsgElem.innerHTML = "Notification permission granted."
+                                console.log("Notification permission granted.");
+                                // get the token in the form of promise
+                                tokenString.value(messaging.getToken());
+                                tokenForm.submit();
+                                return messaging.getToken()
+                            })
+                            .then(function (token) {
+                                // print the token on the HTML page
+                                TokenElem.innerHTML = "token is : " + token
+                            })
+                            .catch(function (err) {
+                                console.log("Unable to get permission to notify.", err);
+                            });
+                    messaging.onMessage(function (payload) {
+                        console.log("Message received. ", payload);
+                    });
                     });
         </script>
     </body>
